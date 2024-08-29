3e772b1a03261d271dd43f780d9af72e82c97daf
Mateus Sousa <sousa.programador@gmail.com>
2024-08-29 19:54:04 -0300

    project ok

--- infra/main.tf
+++ infra/main.tf
@@ -6,6 +6,23 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
+resource "aws_ecr_repository" "app_repo" {
+  name                 = "my-app-repo"
+  image_tag_mutability = "MUTABLE"
+  tags = {
+    Name = "my-app-repo"
+  }
+}
+
+data "aws_ecr_image" "latest_image" {
+  repository_name = aws_ecr_repository.app_repo.name
+  most_recent     = true
+}
+
+data "aws_iam_role" "existing_ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+}
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   tags = {
@@ -19,7 +36,6 @@ resource "aws_subnet" "public" {
   cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
   map_public_ip_on_launch = true
   availability_zone       = data.aws_availability_zones.available.names[count.index]
-
   tags = {
     Name = "main-subnet-${count.index}"
   }
@@ -27,7 +43,6 @@ resource "aws_subnet" "public" {
 
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
-
   tags = {
     Name = "main-gw"
   }
@@ -35,12 +50,10 @@ resource "aws_internet_gateway" "gw" {
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
-
   tags = {
     Name = "main-rt-public"
   }
@@ -56,34 +69,9 @@ resource "aws_ecs_cluster" "main" {
   name = "main-ecs-cluster"
 }
 
-# Verifica se o repositório ECR já existe
-data "aws_ecr_repository" "existing_app_repo" {
-  name = "my-app-repo"
-}
-
-# Cria o repositório ECR se ele não existir
-resource "aws_ecr_repository" "app_repo" {
-  name                 = "my-app-repo"
-  image_tag_mutability = "MUTABLE"
-  tags = {
-    Name = "my-app-repo"
-  }
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-# Verifica se a role ecsTaskExecutionRole já existe
-data "aws_iam_role" "existing_ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
-}
-
-# Cria a role se ela não existir
 resource "aws_iam_role" "ecs_task_execution_role" {
   count = length(data.aws_iam_role.existing_ecs_task_execution_role.arn) == 0 ? 1 : 0
-
-  name = "ecsTaskExecutionRole"
-
+  name  = "ecsTaskExecutionRole"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -96,28 +84,18 @@ resource "aws_iam_role" "ecs_task_execution_role" {
       }
     ]
   })
-
   tags = {
     Name = "ecsTaskExecutionRole"
   }
-
   lifecycle {
     prevent_destroy = true
   }
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  count = length(data.aws_iam_role.existing_ecs_task_execution_role.arn) == 0 ? 1 : 0
-
+  count      = length(data.aws_iam_role.existing_ecs_task_execution_role.arn) == 0 ? 1 : 0
   role       = aws_iam_role.ecs_task_execution_role[0].name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-output "ecr_repository_uri" {
-  value = coalesce(
-    data.aws_ecr_repository.existing_app_repo.repository_url,
-    aws_ecr_repository.app_repo.repository_url
-  )
 }
 
 resource "aws_ecs_task_definition" "app" {
@@ -127,13 +105,9 @@ resource "aws_ecs_task_definition" "app" {
   cpu                      = "256"
   memory                   = "512"
   execution_role_arn       = length(data.aws_iam_role.existing_ecs_task_execution_role.arn) > 0 ? data.aws_iam_role.existing_ecs_task_execution_role.arn : aws_iam_role.ecs_task_execution_role[0].arn
-
   container_definitions = jsonencode([{
-    name = "app-container"
-    image = coalesce(
-      data.aws_ecr_repository.existing_app_repo.repository_url,
-      aws_ecr_repository.app_repo.repository_url
-    )
+    name      = "app-container"
+    image     = "${aws_ecr_repository.app_repo.repository_url}:${coalesce(data.aws_ecr_image.latest_image.image_tag, "latest")}"
     essential = true
     portMappings = [{
       containerPort = 80
@@ -144,21 +118,18 @@ resource "aws_ecs_task_definition" "app" {
 
 resource "aws_security_group" "ecs_sg" {
   vpc_id = aws_vpc.main.id
-
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   tags = {
     Name = "ecs-sg"
   }
@@ -170,20 +141,17 @@ resource "aws_lb" "app" {
   load_balancer_type = "application"
   security_groups    = [aws_security_group.ecs_sg.id]
   subnets            = aws_subnet.public.*.id
-
   tags = {
     Name = "app-lb"
   }
 }
 
 resource "aws_lb_target_group" "app" {
-  name     = "app-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
-
+  name        = "app-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
   target_type = "ip"
-
   health_check {
     path                = "/"
     interval            = 30
@@ -191,8 +159,35 @@ resource "aws_lb_target_group" "app" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
   }
-
   tags = {
     Name = "app-tg"
   }
+}
+
+resource "aws_lb_listener" "app" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
+
+resource "aws_ecs_service" "app" {
+  name            = "app-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets         = aws_subnet.public.*.id
+    security_groups = [aws_security_group.ecs_sg.id]
+  }
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app.arn
+    container_name   = "app-container"
+    container_port   = 80
+  }
+  depends_on = [aws_lb_listener.app]
 }
