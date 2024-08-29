f8ab81890297c959d32048ef70595862688593c2
Mateus Sousa <sousa.programador@gmail.com>
2024-08-29 13:10:48 -0300

    tratamento

--- src/task/infra/nest/dto/create-task.dto.ts
+++ src/task/infra/nest/dto/create-task.dto.ts
@@ -1,11 +1,20 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateTaskUseCase } from '../../../application/use-cases';
 
 export class CreateTaskDto implements CreateTaskUseCase.Input {
+  @ApiProperty({
+    type: 'string',
+    example: 'Testing',
+  })
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({
+    type: 'string',
+    example: 'Testing',
+  })
   @IsNotEmpty()
   @IsString()
   description: string;
@@ -13,6 +22,10 @@ export class CreateTaskDto implements CreateTaskUseCase.Input {
   @IsString()
   image?: string;
 
+  @ApiProperty({
+    type: 'string',
+    example: 'Testing',
+  })
   @IsString()
   status: string;
 }
